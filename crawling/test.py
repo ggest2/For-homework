8f0ebb5bc71793759858346db5a3a9dcfdd0f10e
yoon-janguk <yjohn1209@naver.com>
2024-05-19 23:59:37 +0900

    jango

---
+++ crawling/test.py
@@ -0,0 +1,9 @@
+restaurants = []
+restaurants.append({
+            'name': 'restaurantName',
+            'tags': 'restaurantTags',
+            'rating': 'restaurantRating',
+            'address': 'restaurantAddress',
+            'callNumber': 'restaurantCallNumber'
+        })
+print(restaurants[0]['name'])
