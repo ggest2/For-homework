fb797f7ad961ffcafeedb2223220cc2124642691
yoon-janguk <yjohn1209@naver.com>
2024-05-27 10:00:00 +0900

    django

--- project/mzfinder/models.py
+++ project/mzfinder/models.py
@@ -4,7 +4,8 @@ class Restaurant(models.Model):
     name = models.TextField()
     tags = models.TextField()
     rating = models.IntegerField()
-    adress = models.TimeField()
+    adress = models.TextField()
     callNumber = models.TextField()
+    null = True
     def __str__(self):
-        return self.subject
+        return self.name
