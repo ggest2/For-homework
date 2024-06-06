96d7a32c37b9f58d7edea4775b67695e9ea8d6e4
yoon-janguk <yjohn1209@naver.com>
2024-06-06 22:34:32 +0900

    django

---
+++ project/common/test.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome()
+url = "https://m.map.kakao.com/"
+driver.get(url)
+while True:
+    try:
+        
+        break
+    except:
+        pass
